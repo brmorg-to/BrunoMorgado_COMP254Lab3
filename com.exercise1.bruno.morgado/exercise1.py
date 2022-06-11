ad859f4c604f2fcdeeb7989fa72117015fea6054
Bruno Morgado <brunomorgado@Brunos-iMac.local>
2022-06-11 17:46:41 -0400

    implemented functions 1 and 2

--- com.exercise1.bruno.morgado/exercise1.py
+++ com.exercise1.bruno.morgado/exercise1.py
@@ -3,3 +3,37 @@ Student: Bruno Morgado
 Student# 301-154-898
 '''
 
+'''
+Create a recursive algorithm to compute the product of two positive integers, m and n, using only addition and subtraction.
+Implement the Java or Python code. Hint: You need subtraction to count down from m or n and addition to do the arithmetic
+needed to get the right answer. Check linearSum method from Week 5 examples.
+'''
+
+def positive_integer_multiplier(m, n):
+
+    try:
+        
+        if not type(m) is int or not type(n) is int:
+            raise TypeError("\n****** Only integers are allowed ******\n")
+
+        if m < 0 or n < 0:
+            raise ValueError("\n****** Only positive integers are allowed ******\n")
+
+        if m == 0 or n == 0:
+            return 0 
+
+        return m + positive_integer_multiplier(m, n-1)
+    
+    except Exception as e:
+        
+        print(e)
+
+def main():
+
+    x = positive_integer_multiplier(3, 2)
+
+    print(x)
+
+
+if __name__ == '__main__':
+    main()
--- com.exercise2.bruno.morgado/exercise2.py
+++ com.exercise2.bruno.morgado/exercise2.py
@@ -2,3 +2,40 @@
 Student: Bruno Morgado
 Student# 301-154-898
 '''
+
+'''
+Write a short recursive Java method that determines if a string s is a palindrome,
+that is, it is equal to its reverse. Examples of palindromes include 'racecar' and 'mom'.
+Test the method by asking the user to provide string entries to be checked.
+Hint: Check the equality of the first and last characters and recur (but be careful to return the correct value for both odd and even-length strings).
+'''
+
+def check_palindrome(word):
+
+    if(len(word) < 2):
+        return True
+    elif(word[0] == word[-1]):
+        return check_palindrome(word[1:-1])  
+    
+    return False
+
+
+def main():
+
+    word = ''
+
+    while word != 'q':
+        
+        word = input('\nPlease type the word you want to check. Type "Q" to exit: ').lower().strip()
+        if word == 'q':
+            return
+        answer = check_palindrome(word)
+
+        if(answer):
+            print(f'\n{word} is a palindrome')
+        else:
+            print(f'\n{word} is NOT a palindrome')
+
+
+if __name__ == '__main__':
+    main()
